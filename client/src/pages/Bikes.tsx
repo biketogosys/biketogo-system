181e94b0e14e32816191aa094d34ca501989ea24
biketogosys <ken.kimurabrasil@gmail.com>
2026-05-28 09:34:48 -0300

    Bug ainda aparente
    Aba dados apresenta um bug no mobile

--- client/src/pages/Bikes.tsx
+++ client/src/pages/Bikes.tsx
@@ -414,12 +414,12 @@ function BikeFormDialog({ bike, onClose, onSuccess }: { bike: any | null; onClos
       <DialogContent className="max-w-2xl max-h-[90vh] overflow-y-auto dialog-mobile">
         <DialogHeader><DialogTitle>{isEdit ? "Editar Bicicleta" : "Nova Bicicleta"}</DialogTitle></DialogHeader>
         <Tabs defaultValue="dados">
-          <div className="overflow-x-auto -mx-1 px-1 pb-0.5">
-            <TabsList className="flex w-max min-w-full">
-              <TabsTrigger value="dados" className="flex-1 min-w-[70px] text-xs">Dados</TabsTrigger>
-              <TabsTrigger value="foto" disabled={!currentId} className="flex-1 min-w-[60px] text-xs">Foto</TabsTrigger>
-              <TabsTrigger value="tamanhos" disabled={!currentId} className="flex-1 min-w-[80px] text-xs">Tamanhos</TabsTrigger>
-              <TabsTrigger value="manutencao" disabled={!currentId} className="flex-1 min-w-[90px] text-xs">Manutenção</TabsTrigger>
+          <div className="w-full overflow-x-auto">
+            <TabsList className="w-full min-w-[320px] grid grid-cols-4">
+              <TabsTrigger value="dados" className="text-xs">Dados</TabsTrigger>
+              <TabsTrigger value="foto" disabled={!currentId} className="text-xs">Foto</TabsTrigger>
+              <TabsTrigger value="tamanhos" disabled={!currentId} className="text-xs">Tamanhos</TabsTrigger>
+              <TabsTrigger value="manutencao" disabled={!currentId} className="text-xs">Manutenção</TabsTrigger>
             </TabsList>
           </div>
 
